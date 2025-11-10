d4bde07c13ecd688ab07c86713326b88b8474191
kamel-collab <55155626+kamel-collab@users.noreply.github.com>
2025-11-10 09:54:20 +0100

    d1

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 
-
+//https://github.com/kamel-collab/gemadjezzy-laravel/
 Route::get('/', fn() => view('home'));
 Route::get('/services', fn() => view('services'));
 Route::get('/contact', fn() => view('contact'));
+Route::get('/article/{n}', function ($n) {
+    return view('article')->with('numero', $n);
+});
 
