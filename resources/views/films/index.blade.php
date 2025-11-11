a222d906ccb87dd87312cb34d39d4005f230fe39
kamel-collab <55155626+kamel-collab@users.noreply.github.com>
2025-11-11 11:51:17 +0100

    done

---
+++ resources/views/films/index.blade.php
@@ -0,0 +1,25 @@
+@extends('template')
+
+@section('content')
+  
+<div class="card">
+    <div class="card-header">
+        Films
+    </div>
+    <div class="card-body">
+        <table class="table is-hoverable is-fullwidth">
+            <thead>
+                <tr>
+                    <th>Titre</th>
+                    <th>Description</th>
+                    <th>Année</th>
+                </tr>
+            </thead>
+            <tbody>
+           
+            </tbody>
+
+        </table>
+    </div>
+</div>
+@endsection
